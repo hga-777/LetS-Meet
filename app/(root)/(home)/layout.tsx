8ae3a80c5a6d4c9e43aa5bb0418cd2a61a287d01
Hritik Anand <realhga@gmail.com:--global user.email>
2024-07-19 02:01:34 +0530

    Navbar is created

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,10 +1,11 @@
+import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
 
 const HomeLayout = ({children}:{children:ReactNode}) => {
   return (
     <main className="relative">
-    Navbar
+    <Navbar/>
 
     <div className="flex">
       <Sidebar/>
