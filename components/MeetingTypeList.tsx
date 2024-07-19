01ef2e6528214ae91e2735e96c51fdd13a472cc7
Hritik Anand <realhga@gmail.com:--global user.email>
2024-07-20 02:10:57 +0530

    Meeting Modal has been added

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,11 +3,15 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import HomeCard from './HomeCard';
+import MeetingModal from './MeetingModal';
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >(undefined);
+  const createMeeting = () =>{
+
+  }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
      <HomeCard
@@ -37,6 +41,15 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
      />
+
+     <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   )
 }
