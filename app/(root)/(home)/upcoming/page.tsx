a1639dd7bbd8f97e4e5980427e698a82d505d9d0
Hritik Anand <realhga@gmail.com:--global user.email>
2024-08-29 17:54:47 +0530

    Stream is integrated for video streaming

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import CallList from '@/components/CallList';
 
 const UpcomingPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      Personal Room
-    </section>
-  )
-}
+      <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
 
-export default UpcomingPage
+      <CallList type="upcoming" />
+    </section>
+  );
+};
+
+export default UpcomingPage;
